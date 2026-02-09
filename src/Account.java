98a6c5b829e5a0331eb46b91994cfd3923c54de1
Ismael-276 <ismaelramirez276@gmail.com>
2026-02-09 09:48:20 -0600

    Added methods and made accounts to test the program

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,56 @@
 public class Account {
     private static int lastId = 0;
-    private final int id;
+    private final int id = lastId++;
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+
     public Account(){
 
+        firstName = "unknown";
+        lastName = "unknown";
+        balance = 0;
     }
-    public Account(String firstname, String lastname){
+    public Account(String firstName, String lastName){
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public static int getLastId() {
+        return lastId;
+    }
+
+    public static void setLastId(int lastId) {
+        Account.lastId = lastId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        Account a1 = new Account();
+        Account a2 = new Account("Ismael", "Ramirez");
+        System.out.println(a1.getFirstName());
+        System.out.println(a1.getLastName());
+        System.out.println(a2.getFirstName());
+        System.out.println(a2.getLastName());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
 }
