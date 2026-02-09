3b941f507a652906ec26ced4d62a25f18259c032
Ismael-276 <ismaelramirez276@gmail.com>
2026-02-09 09:35:43 -0600

    Initial Commit

---
+++ src/Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    private static int lastId = 0;
+    private final int id;
+    public Account(){
+
+    }
+    public Account(String firstname, String lastname){
+
+    }
+}
