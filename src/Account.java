04a36280d4045791f0647b1185d152aafae96d5d
Ismael-276 <ismaelramirez276@gmail.com>
2026-02-13 09:18:35 -0600

    Generated javadocs for all elements

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,6 @@
+/**
+ * The type Account.
+ */
 public class Account {
     private static int lastId = 0;
     private final int id = lastId++;
@@ -6,23 +9,46 @@ public class Account {
     private double balance;
 
 
+    /**
+     * Instantiates a new Account.
+     */
     public Account(){
 
         firstName = "unknown";
         lastName = "unknown";
         balance = 0;
+
     }
+
+    /**
+     * Instantiates a new Account.
+     *
+     * @param firstName the first name
+     * @param lastName  the last name
+     */
     public Account(String firstName, String lastName){
         this();
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    /**
+     * Deposit.
+     *
+     * @param amount the amount
+     */
     public void deposit(double amount){
         if(amount < 0)
             throw new IllegalArgumentException("Invalid amount: " + amount + "Must be greater than 0.");
 
 
     }
+
+    /**
+     * Withdrawal.
+     *
+     * @param amount the amount
+     */
     public void withdrawal(double amount){
         if(amount < 0)
             throw new IllegalArgumentException("Invalid amount: " + amount + "Must be greater than 0.");
@@ -33,30 +59,65 @@ public class Account {
 
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets last id.
+     *
+     * @return the last id
+     */
     public static int getLastId() {
         return lastId;
     }
 
+    /**
+     * Sets last id.
+     *
+     * @param lastId the last id
+     */
     public static void setLastId(int lastId) {
         Account.lastId = lastId;
     }
 
+    /**
+     * Gets balance.
+     *
+     * @return the balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets balance.
+     *
+     * @param balance the balance
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
     public void setLastName(String lastName) {
         if (lastName == null || lastName.isEmpty())
             throw new IllegalArgumentException("Invalid last name! Name can't be blank for id: " + id);
@@ -65,10 +126,20 @@ public class Account {
 
     }
 
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
     public void setFirstName(String firstName) {
         if (firstName == null || firstName.isEmpty())
             throw new IllegalArgumentException("Invalid first name! Name can't be blank for id: " + id);
